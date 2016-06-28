e3d84c33c99f628abf82c88b24769313f1dd81d9
rekan <rekanagy06@gmail.com>
2016-06-28 16:36:19 +0200

    init, generate_local, find_student methods implemented, waiting for mentor class to be implemented

--- codecool_class.py
+++ codecool_class.py
@@ -1,6 +1,31 @@
-from mentor import Mentor
+# from mentor import Mentor
 from student import Student
+import random
 
 
 class CodecoolClass:
-    pass
+    def __init__(self, location, year, students):
+        self.location = location
+        self.year = year
+#        self.mentors = []
+        self.students = students
+
+    @classmethod
+    def generate_local(cls):
+        students = [Student(True) for _ in range(5)]
+        year = random.choice([2015, 2016])
+        location = random.choice(["Miskolc", "Budapest", "Krakow"])
+        local_class = CodecoolClass(location, year, students)
+        return local_class
+
+    def find_student_by_full_name(self, full_name):
+        for student in self.students:
+            name = " ".join([student.first_name, student.last_name])
+            if name == full_name:
+                return student
+
+#    def find_mentor_by_full_name(self, full_name):
+#        for mentor in self.mentors:
+#            name = " ".join([mentor.first_name, mentor.last_name])
+#            if name == full_name:
+#                return mentor
