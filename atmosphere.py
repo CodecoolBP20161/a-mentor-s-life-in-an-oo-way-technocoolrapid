fea8b389a05e2426562f45212d0b0b531f4f5644
Balazs Ando <elnesia@gmail.com>
2016-06-28 16:39:21 +0200

    Modify Atmosphere class

--- atmosphere.py
+++ atmosphere.py
@@ -2,13 +2,13 @@ from event import Event
 
 
 class Athomosphere(Event):
+    _id = "morale"
 
-    def __init__(self, moral_change, moral_message, _id, *args, **kwargs):
+    def __init__(self, moral_change, moral_message, *args, **kwargs):
         self.moral_change = moral_change
         self.moral_message = moral_message
-        self._id = _id
 
         super().__init__(*args, **kwargs)
 
     def process_atmosphere(self):
-        return [self.moral_change, self.moral_message, self._id]
+        return [self.moral_change, self.moral_message, Athomosphere._id]
