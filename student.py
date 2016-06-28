9c5cb28324a81457b931f22056b88c25770224e4
rekan <rekanagy06@gmail.com>
2016-06-28 15:56:20 +0200

    student class and methods implemented

--- student.py
+++ student.py
@@ -2,4 +2,16 @@ from person import Person
 
 
 class Student(Person):
-    pass
+    def __init__(self, late, *args, **kwargs):
+        self.late = late
+        self.understood_project = False
+        super().__init__(*args, **kwargs)
+
+    def understanding_project(self, understood):
+        self.understood_project = understood
+        if self.understood_project:
+            self.moral_level += 1
+            print("Student %s understood the project. His/her morale increased by 1." % self.first_name)
+        else:
+            self.moral_level -= 1
+            print("Student %s did not understand the project. His/her morale decreased by 1." % self.first_name)
