2fd5332faaef15ed105cc134800d5fa378afd429
Kristof Galla <gallakristof@gmail.com>
2016-06-29 10:39:52 +0200

    Fix typo

--- mentor.py
+++ mentor.py
@@ -9,7 +9,7 @@ class Mentor(Person):
     def appoint_mentor(self):
         self.responsible = True
         print("{0} {1} is responsible for the project.".format(self.first_name, self.last_name))
-        if randint.random() % 2 == 0:
+        if random.randint(0, 1000) % 2 == 0:
             self.moral_level += 2
             print("This is going to be a great project, let's do it!")
         else:
