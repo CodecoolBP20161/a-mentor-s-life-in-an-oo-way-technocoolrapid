a6d87c5dd0536e22c295ce88110f69b65eb54722
Kristof Galla <gallakristof@gmail.com>
2016-06-29 10:38:15 +0200

    Add moral changes to Mentors if appointed

--- mentor.py
+++ mentor.py
@@ -1,5 +1,5 @@
 from person import Person
-
+import random
 
 class Mentor(Person):
     def __init__(self, *args, **kwargs):
@@ -9,3 +9,9 @@ class Mentor(Person):
     def appoint_mentor(self):
         self.responsible = True
         print("{0} {1} is responsible for the project.".format(self.first_name, self.last_name))
+        if randint.random() % 2 == 0:
+            self.moral_level += 2
+            print("This is going to be a great project, let's do it!")
+        else:
+            self.moral_level -= 2
+            print("Ehh, one more thing to do...")
